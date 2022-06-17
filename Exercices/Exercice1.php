53e3bb5ef83eff09127112df33169f880c7c6605
Erwan NEDELEC <100927435+Erwan-91@users.noreply.github.com>
2022-06-17 16:28:52 +0200

    up

--- Exercices/Exercice1.php
+++ Exercices/Exercice1.php
@@ -1,18 +1,35 @@
-<?php //correction
-$pays = array(
-    'France' => 'Paris',
-    'Allemagne' => 'Berlin',
-    'Italie' => 'Rome',
-    'Espagne' => 'Madrid',
-    'inconnu' => '?',
-);
+<?php
+/* 
+   Vous créez un tableau PHP contenant les pays suivants : France, Italie, Espagne, inconnu, Allemagne auxquels vous associez les valeurs Paris, Rome, Madrid, '?', Berlin.
+
+   Vous parcourez ce tableau pour afficher la phrase "La capitale X se situe en Y" dans un paragraphe (où X remplace la capitale et Y le pays).
+
+   Pour le pays "inconnu" vous afficherez "Ca n'existe pas !" à la place de la phrase précédente. 	
+
+ */
+
+ $pays = array(
+     'France'    => 'Paris', 
+     'Italie'    => 'Rome', 
+     'Espagne'   => 'Madrid', 
+     'inconnu'   => '?', 
+     'Allemagne' => 'Berlin'
+    );
+
+print_r($pays);
+echo count($pays);
+
 
 foreach ($pays as $key => $value) {
-    if($key == 'inconnu'){
-        echo '<p>Ce pays n\'est pas connu</p>';
-    }
-    else{
-        echo '<p>' . $key . ' est le pays de ' . $value . '</p>';
+    // var_dump($pays);
+    // echo '<hr>';
+    // var_dump($key);
+    // echo '<hr>';
+    // var_dump($value);
+    if($key == 'inconnu') {
+        echo '<p>Ce pays n\'existe pas !</p>';
+    } else {
+        echo '<p>La capitale '. $value .' se situe en '. $key .'</p>';
     }
 }
-?>
+var_dump($value);
