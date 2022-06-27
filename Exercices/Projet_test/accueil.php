8bf97a0af10fc23699d4b09ad84667a874582bca
Erwan NEDELEC <100927435+Erwan-91@users.noreply.github.com>
2022-06-27 11:27:59 +0200

    up

--- Exercices/Projet_test/accueil.php
+++ Exercices/Projet_test/accueil.php
@@ -36,6 +36,27 @@
 </nav>
 
 <body>
+<div id="carouselExampleControls" class="carousel slide mb-10" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/psg.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/psg2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./img/psg3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 
     <!-- Bootstrap JavaScript Libraries -->
